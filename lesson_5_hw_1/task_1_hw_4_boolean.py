e0bf4b9f64b067fb88f66b2102c806fef81ccc4d
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-09-12 21:05:05 +0300

    Add files via upload

---
+++ lesson_5_hw_1/task_1_hw_4_boolean.py
@@ -0,0 +1,16 @@
+"""
+Lesson 5, homework 4, task 1 solution.
+"""
+
+
+def main():
+
+    number = int(input('Enter an integer number: ', ))
+    if number % 2 == 0:
+        print('Entered number is an even number!')
+    else:
+        print('Entered number is an odd number!')
+
+
+if __name__ == '__main__':
+    main()
