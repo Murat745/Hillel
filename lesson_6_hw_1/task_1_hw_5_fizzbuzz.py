209f82f742d8f4a254952028d86ce9b47930b983
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-09-19 19:56:34 +0300

    Add files via upload

---
+++ lesson_6_hw_1/task_1_hw_5_fizzbuzz.py
@@ -0,0 +1,14 @@
+"""
+Lesson 6, homework 5, task 1.
+'FizzBuzz' solution.
+"""
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
