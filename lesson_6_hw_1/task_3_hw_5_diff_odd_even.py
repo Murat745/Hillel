dd3c31c224829e509e0c5605f95033c2ec86049b
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-09-26 22:03:06 +0300

    Add files via upload

---
+++ lesson_6_hw_1/task_3_hw_5_diff_odd_even.py
@@ -0,0 +1,21 @@
+"""
+Lesson 6, homework 5, task 3 solution.
+"""
+
+import random
+
+
+def diff_odd_even(num_limit, lower_bound, upper_bound):
+    my_list = [random.randint(lower_bound, upper_bound) for _ in range(num_limit)]
+    print(my_list)  # Запрінтив для наглядності
+    sum_even = sum(i for i in my_list if not i % 2)
+    sum_odd = sum(i for i in my_list if i % 2)
+    return sum_even - sum_odd
+
+
+num_limit = int(input('Введіть num_limit: ', ))
+lower_bound = int(input('Введіть lower_bound: ', ))
+upper_bound = int(input('Введіть upper_bound: ', ))
+result = diff_odd_even(num_limit, lower_bound, upper_bound)
+
+print(result)
