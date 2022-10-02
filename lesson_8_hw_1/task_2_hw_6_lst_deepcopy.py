aaa63bafbd488f1049db7ee16cc7cf861d306e1b
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-10-02 17:05:07 +0300

    Add files via upload

---
+++ lesson_8_hw_1/task_2_hw_6_lst_deepcopy.py
@@ -0,0 +1,13 @@
+"""
+Lesson 7, homework 6, task 2 solution
+Creating a deepcopy of the list.
+"""
+
+lst = ['a', 1, 2.0, ['b']]
+
+
+def copydeep(x):
+    if not isinstance(x, list):
+        return x
+    else:
+        return [copydeep(elem) for elem in x]
