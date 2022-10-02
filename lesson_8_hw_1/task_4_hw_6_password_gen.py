4875f7d2958460efa945c5b7ccb4bd118ec9ad32
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-10-02 20:46:03 +0300

    Add files via upload

---
+++ lesson_8_hw_1/task_4_hw_6_password_gen.py
@@ -0,0 +1,24 @@
+"""
+Lesson 8, homework 6, task 4 solution
+Creating a password generator.
+"""
+
+import random
+import string
+
+print('Your automatically generated password')
+
+length = 8
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = '_'
+total = lower + upper + num + symbols
+temp = random.sample(total, length)
+password = "".join(temp)
+while password:
+    if (any(c.islower() for c in password) and any(c.isupper() for c in password)
+            and any(c.isdigit() for c in password)):
+        break
+
+print(password)
