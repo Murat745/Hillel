ef1e4bbb43fde69cc5d2c5e11ca3e169d3028534
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-10-04 22:25:56 +0300

    Add files via upload

---
+++ lesson_5_hw_1/task_5_hw_4_sign_x.py
@@ -0,0 +1,14 @@
+"""
+Lesson 5, homework 4, Task 5 solution.
+Sign(x) definition.
+"""
+
+
+def sign(x):
+    if x > 0:
+        result = 1
+    elif x < 0:
+        result = -1
+    else:
+        result = 0
+    return result
