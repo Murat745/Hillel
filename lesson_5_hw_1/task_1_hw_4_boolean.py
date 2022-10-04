d3d64f67494b62bee7d0421eb9fb88057fd82c63
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-10-04 20:46:14 +0300

    Add files via upload

---
+++ lesson_5_hw_1/task_1_hw_4_boolean.py
@@ -0,0 +1,11 @@
+"""
+Lesson 5, homework 4, task 1 solution.
+"""
+# returns True if number is odd and False if number is even
+
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        return False
+    else:
+        return True
