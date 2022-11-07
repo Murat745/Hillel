60e0766a24f6c700caa3bdebe5f7ca5bb0e585e1
Ivan Muratov <111700768+Murat745@users.noreply.github.com>
2022-11-07 22:06:48 +0200

    Create task_1_hw_9_reading_a_text_file_to_the_list.py

---
+++ lesson_12_hw_9/task_1_hw_9_reading_a_text_file_to_the_list.py
@@ -0,0 +1,19 @@
+"""
+Lesson 12, homework 9, task 1 solution.
+Reading a text file to the list.
+"""
+
+
+def from_file_to_list(file_name):
+    list_of_data = []
+    for i in open(str(file_name)):
+        list_of_data.append(i)
+
+    for i in range(len(list_of_data)):
+        list_of_data[i] = list_of_data[i][1:]
+        list_of_data[i] = list_of_data[i].strip()
+
+    return list_of_data
+
+
+print(from_file_to_list("domains.txt"))
